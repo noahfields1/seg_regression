56acfedb13044d98b44bc1899eff24a4becba723
gabriel <gdmaher@stanford.edu>
2018-12-04 08:54:56 -0800

    model factory

---
+++ components/models/rf.py
@@ -0,0 +1,41 @@
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+
+from base.model import AbstractModel
+
+class RFModel(AbstractModel):
+    def setup(self):
+        n_estimators = self.config['N_ESTIMATORS']
+        self.rf      = RandomForestRegressor(n_estimators=n_estimators)
+        self.name = self.config['NAME']
+    def predict(self, x):
+        """
+        x - array [HxW] or [NxHxW]
+        """
+        S = x.shape
+        if len(S) == 2:
+            return self._predict_single(x)
+        else:
+            return self._predict(x)
+
+    def _predict_single(self, x):
+        S  = x.shape
+        x_ = x.reshape([1,s[0]*s[1]])
+        p  = self.rf.predict(x_)
+        return p[0]
+
+    def _predict(self, X):
+        S  = X.shape
+        X_ = X.reshape([-1,S[1]*S[2]])
+        return self.rf.predict(X_)
+
+    def train(self, X, Y):
+        S  = X.shape
+        X_ = X.reshape([-1,S[1]*S[2]])
+        self.rf.fit(X_,Y)
+
+    def save(self, path):
+        joblib.dump(self.rf,path+'/{}.joblib'.format(self.name))
+
+    def load(self, path):
+        self.rf = joblib.load(path+'/{}.joblib'.format(self.name))
--- factories/model_factory.py
+++ factories/model_factory.py
@@ -1,1 +1,12 @@
+import components.models.rf as rf
+
 def get_model(config):
+    if not "MODEL" in config:
+        raise RuntimeError("MODEL key missing from config")
+
+    mod = config["MODEL"]
+
+    if mod == "rf2d":
+        return rf.RFModel(config)
+    else:
+        raise RuntimeError("Unrecognized model type {}".format(mod))
