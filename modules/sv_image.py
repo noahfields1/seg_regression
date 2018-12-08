72120679ca1ae114bd5fa1efd9c5b891f19d3707
gabriel <gdmaher@stanford.edu>
2018-12-08 10:51:31 -0800

    2d contours

--- modules/sv_image.py
+++ modules/sv_image.py
@@ -1,8 +1,24 @@
 import modules.vascular_data as sv
+import numpy as np
 
 class Image(object):
     def __init__(self, filename):
         self.image = sv.read_mha(filename)
 
-    def get_reslice(self, p, n, v, spacing):
-        return sv.getImageReslice(self.image, p, n, v, spacing, asnumpy=True)
+    def set_spacing(self, spacing):
+        self.spacing = spacing
+
+    def set_reslice_ext(self, ext):
+        self.ext = [ext-1]*2
+
+    def get_reslice(self, p, n, v):
+        return sv.getImageReslice(self.image, self.ext,
+            p, n, v, self.spacing, asnumpy=True)
+
+    def get_reslices(self, plist, nlist, vlist):
+        reslices = []
+        for p,n,v in zip(plist,nlist,vlist):
+            x = self.get_reslice(p,n,v)
+            reslices.append(x.copy())
+
+        return np.array(reslices)
--- scripts/build_cv_model.py
+++ scripts/build_cv_model.py
@@ -11,16 +11,19 @@ import factories.model_factory as model_factory
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-i', description='medical image file')
-parser.add_argument('-p', description='.paths file')
-parser.add_argument('-c', description='model config file')
-parser.add_argument('-o', description='output directory')
+parser.add_argument('-i', help='medical image file')
+parser.add_argument('-p', help='.paths file')
+parser.add_argument('-c', help='model config file')
+parser.add_argument('-o', help='output directory')
 
 args = parser.parse_args()
 
 image     = sv_image.Image(args.i)
 path_dict = sv.parsePathFile(args.p)
 config    = io.load_yaml(args.c)
+
+image.set_spacing(config['SPACING'])
+image.set_reslice_ext(config['CROP_DIMS'])
 
 output_dir = os.path.abspath(args.o)
 name_dir   = os.path.join(output_dir, config['NAME'])
@@ -30,3 +33,24 @@ model = model_factory.get_model(config)
 
 for p in [output_dir, name_dir, group_dir]:
     if not os.path.isdir(p): os.mkdir(p)
+
+for path_id, path in path_dict.items():
+        path_name = path['name']
+        path_points = path['points']
+
+        p = [x[:3] for x in path_points]
+        n = [x[3:6] for x in path_points]
+        v = [x[6:] for x in path_points]
+
+        X = image.get_reslices(p,n,v)
+
+        C = model.predict(X)
+
+        path_dict[path_id]['contours_2d'] = C
+
+for path_id, path in path_dict.items():
+    #TODO loft into 3d groups
+
+for path_id, path in path_dict.items():
+    #TODO write to files
+    #TODO write groups toc file
