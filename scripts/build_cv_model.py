f5b3e12640c16b104c5198218fc865ea976c9e22
gabriel <gdmaher@stanford.edu>
2018-12-09 13:11:23 -0800

    build model

--- scripts/build_cv_model.py
+++ scripts/build_cv_model.py
@@ -38,28 +38,45 @@ for p in [output_dir, name_dir, group_dir]:
     if not os.path.isdir(p): os.mkdir(p)
 
 for path_id, path in path_dict.items():
-        path_name = path['name']
-        path_points = path['points']
+    path_name = path['name']
+    path_points = path['points']
 
-        p = [x[:3] for x in path_points]
-        n = [x[3:6] for x in path_points]
-        v = [x[6:] for x in path_points]
+    p = [x[:3] for x in path_points]
+    n = [x[3:6] for x in path_points]
+    v = [x[6:] for x in path_points]
 
-        X = image.get_reslices(p,n,v)
-        mu = np.mean(X,axis=(1,2))[:,np.newaxis,np.newaxis]
-        std = np.std(X,axis=(1,2))[:,np.newaxis,np.newaxis]
-        X = (1.0*X-mu)/(std+1e-3)
-        
-        C = model.predict(X)
-        C = np.array([vessel_regression.pred_to_contour(c) for c in C])
-        C = C*config['CROP_DIMS']*config['SPACING']/2
-        
-        path_dict[path_id]['contours_2d'] = C
+    X = image.get_reslices(p,n,v)
+    mu = np.mean(X,axis=(1,2))[:,np.newaxis,np.newaxis]
+    std = np.std(X,axis=(1,2))[:,np.newaxis,np.newaxis]
+    X = (1.0*X-mu)/(std+1e-3)
 
-        C3 = np.array([sv.denormalizeContour(c_,p_,n_,v_) for c_,p_,n_,v_ in zip(C,p,n,v)])
+    C = model.predict(X)
+    C = np.array([vessel_regression.pred_to_contour(c) for c in C])
+    C = C*config['CROP_DIMS']*config['SPACING']/2
 
-        path_dict[path_id]['contours_3d'] = C3
+    path_dict[path_id]['contours_2d'] = C
 
-#for path_id, path in path_dict.items():
-#TODO write to files
-#TODO write groups toc file
+    C3 = np.array([sv.denormalizeContour(c_,p_,n_,v_) for c_,p_,n_,v_ in zip(C,p,n,v)])
+
+    path_dict[path_id]['contours_3d'] = C3
+
+for path_id, path in path_dict.items():
+
+    print( "saving groups file")
+    name   = path['name']
+    points = path['points']
+    print( name)
+
+    f = open(os.path.join(args.o,name),'w')
+
+    for i,p in enumerate(points):
+        c = path['contours_3d'][i]
+        pos = int(p[0])
+        f.write('/group/{}/{}\n'.format(name,pos))
+        f.write(str(pos) +'\n')
+        f.write('posId {}\n'.format(pos))
+        for j in range(c.shape[0]):
+            f.write('{} {} {}\n'.format(c[j][0],c[j][1],c[j][2]))
+        f.write('\n')
+
+    f.close()
