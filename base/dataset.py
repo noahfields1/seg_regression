356208361e8aeda48bd884754313fbb887546485
gabriel <gdmaher@stanford.edu>
2018-12-30 10:46:20 -0800

    abstract dataset

---
+++ base/dataset.py
@@ -0,0 +1,9 @@
+class AbstractDataset(object):
+    def __init__(self, raw_data):
+        self.raw_data = raw_data
+    def configure(self,config):
+        pass
+    def get(index):
+        pass
+    def get_batch(batch_size=16):
+        pass
