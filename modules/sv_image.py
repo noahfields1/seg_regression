b06e9eb7e74b927fb23549a0fb0ef60446e524fa
gabriel <gdmaher@stanford.edu>
2019-02-20 09:03:08 -0800

    image reader

--- modules/sv_image.py
+++ modules/sv_image.py
@@ -1,9 +1,29 @@
 import modules.vascular_data as sv
 import numpy as np
+import os
 
 class Image(object):
     def __init__(self, filename):
-        self.image = sv.read_mha(filename)
+
+        if not os.path.exists(filename):
+            raise RuntimeError("path {} does not exist".format(filename))
+            
+        if ".mha" in filename:
+            self.image = sv.read_mha(filename)
+
+        elif ".vti" in filename:
+            self.image = sv.read_vti(filename)
+
+        elif ".dcm" in filename:
+            d = os.path.dirname(os.path.abspath(filename))
+            self.image = sv.read_dicom(d)
+
+        elif os.path.isdir(filename):
+            #assumed dicom dir
+            self.image = sv.read_dicom(filename)
+
+        else:
+            raise RuntimeError("unrecognized image format {}".format(filename))
 
     def set_spacing(self, spacing):
         self.spacing = spacing
