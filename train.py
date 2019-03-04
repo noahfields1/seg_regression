03147aa37de000b3e2aaec93e7ca7c21d3cb90cb
marsdenlab <gdmaher@stanford.edu>
2019-03-04 14:33:46 -0800

    train file

---
+++ train.py
@@ -0,0 +1,37 @@
+import modules.io as io
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('config_file')
+parser.add_argument('data_key')
+
+args = parser.parse_args()
+
+config = io.load_yaml(args.config_file)
+
+#dataset
+import factories.dataset_factory as dataset_factory
+data    = dataset_factory.get(config, DATA_KEY)
+
+#model
+import factories.model_factory as model_factory
+model = model_factory.get(config)
+
+#preprocessor
+import factories.preprocessor_factory as prepro_factory
+preprocessor = prepro_factory.get(config)
+
+#trainer
+import factories.trainer_factory as trainer_factory
+trainer = trainer_factory.get(config)
+trainer.setup_directories()
+
+trainer.set_data(data, DATA_KEY)
+trainer.set_preprocessor(preprocessor)
+
+trainer.set_model(model)
+
+trainer.train()
+
+trainer.save()
