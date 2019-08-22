13e2e7fa34ec42a8bf77c2d8061ec10aa4bf48af
gabriel <gdmaher@stanford.edu>
2019-08-22 12:00:31 -0700

    model script

--- scripts/computeSegmentations.py
+++ scripts/computeSegmentations.py
@@ -22,14 +22,18 @@ if not os.path.isfile(args.config):
         args.config
     ))
 
+if not os.path.isfile(args.image_file):
+    raise RuntimeError("image file {} does not exist".format(
+        args.image_file
+    ))
+
 net = SVWrapper(args.config)
+net.set_image(args.image_file)
 
 p_file = open(args.points_file).readlines()
 p_file = [p.replace('\n','') for p in p_file]
 
 points = [np.array([float(x) for x in p.split(',')]) for p in p_file]
-
-contours_3d.append(c_)
 
 print( "saving groups file")
 name = args.points_file.split('/')[-1].replace('.txt','')
@@ -38,7 +42,8 @@ f = open(args.output_dir+'/'+name,'w')
 
 for i,p in enumerate(points):
 
-    c = contours_3d[i]
+    contour_3d = net.segment_normal(p[:3], p[3:6], p[6:])
+
     pos = int(p[0])
     f.write('/group/{}/{}\n'.format(name,pos))
     f.write(str(pos) +'\n')
