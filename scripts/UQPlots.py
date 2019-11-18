9ab64b46fda9d5ebefaa9c31e27a26f61f7a8890
marsdenlab <gdmaher@stanford.edu>
2019-11-18 14:04:51 -0800

    update

--- scripts/UQPlots.py
+++ scripts/UQPlots.py
@@ -14,3 +14,17 @@ reader.SetFileName(fn)
 reader.Update()
 
 pd = reader.GetOutput()
+
+########################
+# Get Cell
+########################
+coord = [0.0]*3
+tol   = 0.05
+
+cell_id = sv.vtkPdFindCellId(pd, coord, tol)[0]
+cell    = pd.GetCell(cell_id)
+ids     = cell.GetPointIds()
+print(ids.GetId(0), ids.GetId(1), ids.GetId(2))
+
+data = pd.GetPointData().GetArray(label)
+values = [data.GetValue(ids.GetId(i)) for i in range(3)]
