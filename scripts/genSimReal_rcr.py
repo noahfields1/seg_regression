785c09895300e681cb6c0311be87d73563f29b9e
marsdenlab <gdmaher@stanford.edu>
2020-01-13 18:11:46 -0800

    update

--- scripts/genSimReal_rcr.py
+++ scripts/genSimReal_rcr.py
@@ -28,21 +28,13 @@ GROUPS       = cfg['groups']
 GROUPS_FILES = cfg['groups_files']
 TOTAL_R      = cfg['total_r']
 
-SOLVER_FILE  = cfg['solver_file']
-SOLVER_LINES = open(SIM_DIR+'/'+SOLVER_FILE,'r').readlines()
+RCR_FILE  = cfg['rcr_file']
+RCR_LINES = open(SIM_DIR+'/'+RCR_FILE,'r').readlines()
 
-INLET = cfg['inlet']
+C = cfg["capacitance"]
 
 for g,mo in zip(GEN_DIRS,MODEL_DIRS):
     for i in range(NUM_MODELS):
-
-        g_inlet = io.load_json(mo+'/'+str(i)+'/'+INLET['group'])
-        c_inlet = np.array(g_inlet[INLET['point']])
-        A_inlet = sv.calc_area_3d(c_inlet)
-        flow_inlet = INLET['flow_val']*1.0/A_inlet
-        l_inlet = "0 {}\n1 {}".format(flow_inlet, flow_inlet)
-        d = {"area":A_inlet, "flow":flow_inlet}
-        io.write_json(d,mo+'/'+str(i)+'/inlet.json')
 
         group_areas = {}
         for group,group_fn in zip(GROUPS,GROUPS_FILES):
@@ -68,7 +60,17 @@ for g,mo in zip(GEN_DIRS,MODEL_DIRS):
 #        R = TOTAL_R/np.prod([v for k,v in ratios.items()])
         R = TOTAL_R*len(GROUPS)
         for k,v in ratios.items(): resistances[k]=v*R
-        io.write_json(resistances,mo+'/'+str(i)+'/'+'resistances.json')
+        io.write_json(resistances,mo+'/'+str(i)+'/'+'resistances_rcr.json')
+
+
+        for k,v in group_areas.items(): ratios[k] = v
+        total_ratios = 0
+        for k,v in ratios.items(): total_ratios+=v
+        for k in ratios: ratios[k] = ratios[k]/total_ratios*len(GROUPS)
+
+        capacitances = {}
+        for k,v in ratios.items(): capacitances[k]=v*C
+        io.write_json(capacitances,mo+'/'+str(i)+'/'+'capacitances.json')
 
         for me in MESH_DIRS:
             print(i,me)
@@ -78,18 +80,21 @@ for g,mo in zip(GEN_DIRS,MODEL_DIRS):
                 os.system('rm -r {}'.format(me_dir+'/'+SIM_NAME))
                 os.system('cp -r {} {}'.format(SIM_DIR,me_dir))
 
-                new_solv_file = me_dir+'/'+SIM_NAME+'/'+SOLVER_FILE
+                new_solv_file = me_dir+'/'+SIM_NAME+'/'+RCR_FILE
 
                 with open(new_solv_file,'w') as f:
-                    for l in SOLVER_LINES:
-                        if '$' in l:
-                            for k,v in resistances.items():
-                                key = '$'+k+'$'
-                                l = l.replace(key,str(resistances[k]))
-                        f.write(l)
+                    for l in RCR_LINES:
+                        lk = l.replace('\n','')
+                        if lk in resistances:
+                            r = resistances[lk]
+                            c = capacitances[lk]
 
-                # new_flow_file = me_dir+'/'+SIM_NAME+'/'+INLET['file']
-                # with open(new_flow_file,'w') as f:
-                #     f.write(l_inlet)
+                            f.write(str(r/10)+'\n')
+                            f.write(str(c)+'\n')
+                            f.write(str(r)+'\n')
+
+                        else:
+                            f.write(l)
+
             except:
                 print("failed")
