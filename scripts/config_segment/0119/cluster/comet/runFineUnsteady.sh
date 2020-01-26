a7a52332ba182ca9d85dd5e4cae938f517259d7a
marsdenlab <gdmaher@stanford.edu>
2020-01-26 14:30:22 -0800

    update

---
+++ scripts/config_segment/0119/cluster/comet/runFineUnsteady.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Name of your job
+# CHANGE THIS JOB NAME
+#SBATCH --job-name=fineSteady
+#SBATCH --partition=shared
+
+# Specify the name of the output file. The %j specifies the job ID
+# CHANGE THIS JOB OUTPUT FILE NAME
+#SBATCH --output=log.o%j
+
+# Specify a name of the error file. The %j specifies the job ID
+# CHANGE THIS JOB ERROR FILE NAME
+#SBATCH --error=log.e%j
+
+# The walltime you require for your simulation
+#SBATCH --time=24:00:00
+
+# Number of nodes you are requesting for your job. You can have 24 processors per node, so plan accordingly
+# CAN CHANGE THE NUMBER OF NODES
+# 20,000 elements/processor is a good rule of thumb for scaling for FSI. (i.e. 4million FSI mesh is about 192 processors = 8 nodes)
+# 40,0000 elements/processor for rigid (i.e. 4million rigid mesh is about 96 processors = 4 nodes)
+#SBATCH --nodes=1
+
+# Number of processors per node
+#SBATCH --ntasks-per-node=23
+
+# Send an email to this address when you job starts and finishes
+# CHANGE THIS EMAIL ADDRESS
+#SBATCH --mail-user=contact.gabriel.maher@gmail.com
+#SBATCH --mail-type=all
+
+# Modules
+module purge
+module load gnu/7.2.0
+module load lapack
+#module load gnu openmpi_ib
+module load mvapich2_ib/2.3.2
+module load python
+module load scipy
+module load boost
+
+
+# Name of the executable you want to run
+/home/gdmaher/svSolver/svpre.exe model_sim.svpre
+ibrun /home/gdmaher/svSolver/svsolver-mpich.exe
+/home/gdmaher/svSolver/svpost.exe -indir 23-procs_case -outdir . -start 1500 -stop 2000 -incr 5 -vtu all_results.vtu -vtp all_results.vtp -vtkcombo -all
