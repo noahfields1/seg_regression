d661d2cd023c72fb0a9e177edd5872f0db53c4d3
gabriel <gdmaher@stanford.edu>
2020-04-03 15:50:18 -0700

    update

--- scripts/segmentMesh_tetgen.py
+++ scripts/segmentMesh_tetgen.py
@@ -90,7 +90,9 @@ msh.NewObject('mesh')
 #Load Model
 msh.LoadModel(EXTERIOR_FILE)
 if BOUNDARY_LAYER:
+    print("get boundary faces")
     msh.GetBoundaryFaces(60.)
+    print("set walls")
     msh.SetWalls(WALL_IDS)
 #msh.GetBoundaryFaces(80)
 #Create new mesha
