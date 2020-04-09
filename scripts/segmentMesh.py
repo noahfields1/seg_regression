6f0b4e17381bb2a6b017bf5ded1b5e0f52a0ff59
gabriel <gdmaher@stanford.edu>
2020-04-09 11:45:44 -0700

    update

--- scripts/segmentMesh.py
+++ scripts/segmentMesh.py
@@ -117,7 +117,7 @@ for name,path,group in zip(NAMES, PATHS, GROUPS):
 ################################################################################
 # 3. Loft Segmentations
 ################################################################################
-numOutPtsInSegs = 15
+numOutPtsInSegs = 33
 numOutPtsAlongLength = 200
 numPtsInLinearSampleAlongLength = 240
 numLinearPtsAlongLength = 120
