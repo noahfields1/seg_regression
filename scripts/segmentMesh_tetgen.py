46873541d180c341d62eb3db6b82ce605b7783e0
gabriel <gdmaher@stanford.edu>
2020-05-02 16:27:00 -0700

    update

--- scripts/segmentMesh_tetgen.py
+++ scripts/segmentMesh_tetgen.py
@@ -144,9 +144,9 @@ print("pre boundary layer")
 if BOUNDARY_LAYER:
     print("boundary layer")
     for v in WALL_IDS:
-        msh.SetBoundaryLayer(0,int(v),0,5,[0.75,0.65,0])
+        msh.SetBoundaryLayer(0,int(v),0,4,[0.75,0.65,0])
     for v in CAP_IDS:
-        msh.SetBoundaryLayer(0,int(v),0,5,[0.75,0.65,0])
+        msh.SetBoundaryLayer(0,int(v),0,4,[0.75,0.65,0])
 
 # RADIUS = True
 # if RADIUS_MESH:
